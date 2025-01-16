7a9a3b601f73083cff157fb23074378ac6e75004
Jessica Sumargo <>
2025-01-16 14:49:30 +0100

    CRUD skeleton for project

---
+++ src/controllers/projectController.ts
@@ -0,0 +1,11 @@
+//import { Request, Response } from 'express';
+//import { v4 as uuidv4 } from 'uuid';
+//import { Project } from '../models/project.model';
+
+//const projects: Project[] = [];
+
+export const createProject = null;
+export const getAllProjects = null;
+export const getProjectById = null;
+export const updateProject = null;
+export const deleteProject = null;
