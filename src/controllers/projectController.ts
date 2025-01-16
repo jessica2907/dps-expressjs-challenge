b5e7bf328cc47ec2c9e977085fa969e914b2451b
Jessica Sumargo <>
2025-01-16 15:16:55 +0100

    first implementation attempt of createProject and getAllProjects

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -1,11 +1,23 @@
-//import { Request, Response } from 'express';
-//import { v4 as uuidv4 } from 'uuid';
-//import { Project } from '../models/project.model';
+import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import db from '../services/db.service';
 
-//const projects: Project[] = [];
+export const createProject = (req: Request, res: Response) => {
+	const { name, description } = req.body;
+	const id = uuidv4();
+	db.run(
+		'INSERT INTO projects (id, name, description) VALUES (@id, @name, @description)',
+		{ id, name, description },
+	);
 
-export const createProject = null;
-export const getAllProjects = null;
-export const getProjectById = null;
-export const updateProject = null;
-export const deleteProject = null;
+	res.status(201).json({ id, name, description });
+};
+
+export const getAllProjects = (req: Request, res: Response) => {
+	const projects = db.query('SELECT * FROM projects');
+	res.status(200).json(projects);
+};
+
+//export const getProjectById = null;
+//export const updateProject = null;
+//export const deleteProject = null;
---
+++ src/routes/projects.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import {
+	createProject,
+	getAllProjects,
+} from '../controllers/projectController';
+
+const router = Router();
+
+router.post('/', createProject);
+router.get('/', getAllProjects);
+
+export default router;
