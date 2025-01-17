a2f48234d3738c8064b94950814bfb8143b8cf23
Jessica Sumargo <>
2025-01-17 17:58:13 +0100

    completing project CRUD

--- src/controllers/projectController.ts
+++ src/controllers/projectController.ts
@@ -18,6 +18,41 @@ export const getAllProjects = (req: Request, res: Response) => {
 	res.status(200).json(projects);
 };
 
-//export const getProjectById = null;
-//export const updateProject = null;
-//export const deleteProject = null;
+export const getProjectById = (req: Request, res: Response) => {
+	const project = db.query('SELECT * FROM projects WHERE id = @id', {
+		id: req.params.id,
+	});
+
+	if (project.length === 0) {
+		return res.status(404).json({ message: 'Project not found' });
+	}
+
+	res.status(200).json(project[0]);
+};
+
+export const updateProject = (req: Request, res: Response) => {
+	const { name, description } = req.body;
+
+	const result = db.run(
+		'UPDATE projects SET name = @name, description = @description WHERE id = @id',
+		{ id: req.params.id, name, description },
+	);
+
+	if (result.changes === 0) {
+		return res.status(404).json({ message: 'Project not found' });
+	}
+
+	res.status(200).json({ id: req.params.id, name, description });
+};
+
+export const deleteProject = (req: Request, res: Response) => {
+	const result = db.run('DELETE FROM projects WHERE id = @id', {
+		id: req.params.id,
+	});
+
+	if (result.changes === 0) {
+		return res.status(404).json({ message: 'Project not found' });
+	}
+
+	res.status(204).send();
+};
